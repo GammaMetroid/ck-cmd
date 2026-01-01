7438b4d3b2a299b705798edfed47cde487fe9ef3
GammaMetroid <101038501+GammaMetroid@users.noreply.github.com>
2026-01-01 13:56:39 -0800

    add vertex welding to cms builder

--- src/core/HKXWrangler.cpp
+++ src/core/HKXWrangler.cpp
@@ -3348,6 +3348,8 @@ hkRefPtr<hkpShape> HKXWrapper::build_shape(
 	{
 		hkpCompressedMeshShapeBuilder			shapeBuilder;
 		shapeBuilder.m_stripperPasses = 5000;
+		shapeBuilder.m_weldVertices = true;
+		shapeBuilder.m_weldTolerance = 0.001f;
 		hkpCompressedMeshShape* pCompMesh = shapeBuilder.createMeshShape(0.001f, hkpCompressedMeshShape::MATERIAL_SINGLE_VALUE_PER_CHUNK);
 		pCompMesh->m_namedMaterials.setSize(materials.size());
 		for (int i = 0; i < materials.size(); i++)
