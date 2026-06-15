fa4ad2f852ae8eea63f34d1a560e9d6260fb6c99
GammaMetroid <101038501+GammaMetroid@users.noreply.github.com>
2026-06-14 20:13:27 -0700

    update collision handling (#8)
    * add vertex welding to cms builder
    
    * increase bits per index for cms
    
    * revert
    
    * increase stripper passes for cms
    
    * revert
    
    * x10 weld tolerance and quantization error
    
    * definitely not that!
    
    * try some other parameters
    
    * increase overlap ratio
    
    * revert
    
    * try overlap ratio > 1
    
    * single stripper pass
    
    * add capability for calculation of material type value for arbitrary types
    
    * use hkcrc function directly instead
    
    * try fixing errors
    
    * instantiate hkcrc object?
    
    * add missing collision materials
    
    * add Skywind metal stairs material

--- src/core/HKXWrangler.cpp
+++ src/core/HKXWrangler.cpp
@@ -3347,7 +3347,9 @@ hkRefPtr<hkpShape> HKXWrapper::build_shape(
 	if (ends_with(name, "_mesh"))
 	{
 		hkpCompressedMeshShapeBuilder			shapeBuilder;
-		shapeBuilder.m_stripperPasses = 5000;
+		shapeBuilder.m_stripperPasses = 1;
+		shapeBuilder.m_weldVertices = true;
+		shapeBuilder.m_weldTolerance = 0.001f;
 		hkpCompressedMeshShape* pCompMesh = shapeBuilder.createMeshShape(0.001f, hkpCompressedMeshShape::MATERIAL_SINGLE_VALUE_PER_CHUNK);
 		pCompMesh->m_namedMaterials.setSize(materials.size());
 		for (int i = 0; i < materials.size(); i++)
